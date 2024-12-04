cd855986f63e6d07260ad39b197735bea483c380
Marv <bmurfield@gmail.com>
2024-12-04 16:21:10 -0600

    added working component tests

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,40 +1,59 @@
-import React from 'react';
-import { mount } from 'cypress/react';
-import Quiz from '../../client/src/components/Quiz'; 
-import questions from '../../cypress/fixtures/questions.json';
+import React from "react";
+import { mount } from "cypress/react";
+import Quiz from "../../client/src/components/Quiz";
+import questions from "../../cypress/fixtures/questions.json";
 
-describe('Quiz Component', () => {
+describe("Quiz Component", () => {
   beforeEach(() => {
-    cy.intercept({
-      method: 'GET',
-      url: '/api/questions/random',
-    }, {
-      fixture: "questions.json",
-      statusCode: 200
-    }).as("randomQuestion")   
-    cy.fixture("questions.json").as("randomQuestion")
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/api/questions/random",
+      },
+      {
+        fixture: "questions.json",
+        statusCode: 200,
+      }
+    ).as("randomQuestion");
   });
 
-  it('renders the first question correctly', () => {
-    cy.mount(<Quiz/>);
+  // TODO:
+  it("renders the first question correctly", () => {
+    cy.mount(<Quiz />);
     cy.get("button").contains("Start Quiz").click();
- 
-    cy.log(this.randomQuestion)
-    cy.get('h2').should('contain', this.randomQuestion[0].question)
+    cy.wait("@randomQuestion");
+    cy.get("h2").should("contain", questions[0].question);
+  });
+
+  // TODO:
+  it("displays the correct answers for the first question", () => {
+    cy.mount(<Quiz />);
+    cy.get("button").contains("Start Quiz").click();
+
+    // Wait for the mocked API request to complete
+    cy.wait("@randomQuestion");
+
+    // Verify all the answers are displayed
+    questions[0].answers.forEach((answer, index) => {
+      cy.get(".alert-secondary").eq(index).should("contain", answer.text);
+    });
+  });
+  // TODO:
+  it("advances to the next question when an answer is selected", () => {
+    cy.mount(<Quiz />);
+    cy.get("button").contains("Start Quiz").click();
+
+    // Wait for the mocked API request to complete
+    cy.wait("@randomQuestion");
+
+    // Select the first answer
+    cy.get(".btn-primary").eq(0).click();
+
+    // Verify the next question (if available)
+    if (questions.length > 1) {
+      cy.get("h2").should("contain", questions[1].question);
+    } else {
+      cy.get("h2").should("contain", "Quiz Completed");
     }
-   
-  );
-
-//   it('displays the correct answers for the first question', () => {
-//     mount(<Quiz questions={questions} />);
-//     questions[0].answers.forEach((answer) => {
-//       cy.contains(answer.text).should('be.visible');
-//     });
-//   });
-
-//   it('advances to the next question when an answer is selected', () => {
-//     mount(<Quiz questions={questions} />);
-//     cy.contains(questions[0].answers[0].text).click();
-//     cy.contains(questions[1].question).should('be.visible');
-//   });
+  });
 });
